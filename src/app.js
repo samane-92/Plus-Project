a25e2916669882fc4b10e70b578950bef05033d2
samane-92 <131759514+samane-92@users.noreply.github.com>
2023-05-08 12:38:35 +0430

    add search engine

--- src/app.js
+++ src/app.js
@@ -21,16 +21,38 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let speedElement = document.querySelector("#speed");
   let dateElement = document.querySelector("#date");
+  let imgElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   h1.innerHTML = response.data.name;
   liElement.innerHTML = response.data.weather[0].main;
   humidityElement.innerHTML = response.data.main.humidity;
   speedElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  imgElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  imgElement.setAttribute("alt", response.data.weather[0].descriptiob);
 }
+
+function displayFarenhitTemperature(event) {
+  event.preventDefault();
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", displayTemperature, formatDate);
 
 let apiKey = "cd4fdbb5dd7dfb75e8adcfc668859d3b";
 let city = "Paris";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+
+function convert(temperature) {
+  displayTemperature(response);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp * 9.5 + 32);
+}
+
+let fahrenheitLink = document.querySelector("fahrenheit-link");
+fahrenheitLink.addEventListener = ("click", displayFarenhitTemperature);
